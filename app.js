ff5a3b275f68a81936e3ff91229e05d488172912
Ochuko Angozi <ochuks33@yahoo.com>
2022-12-02 08:10:52 -0500

    added javascript functionality. mozilla firefox might have a caching bug

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("h")
+//selectors
+const advice_id = document.querySelector(".advice_id");
+const button = document.querySelector(".icon");
+
+ 
+
+const API_URL = "https://api.adviceslip.com/advice";
+
+
+
+//event listeners
+
+
+
+//functions
+
+async function start() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    const { id, advice } = data.slip;
+    console.log(id, advice);
+    document.querySelector(".main_advice").textContent = advice;
+    document.querySelector(".advice_id").textContent = id;
+    
+}
+
+button.addEventListener("click", start);
+
